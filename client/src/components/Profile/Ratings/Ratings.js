401fcd727d689273b1ac4ac98ccbabe11ac64c8b
gabivlj <gabitriqui@gmail.com>
2019-06-14 00:30:38 +0200

    Fixed loading on ratings of profile

--- client/src/components/Profile/Ratings/Ratings.js
+++ client/src/components/Profile/Ratings/Ratings.js
@@ -1,71 +1,92 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios'
+import axios from 'axios';
 import Rating from './Rating';
 import PropTypes from 'prop-types';
 import { CircularProgress } from '@material-ui/core';
 
 const propTypes = {
-  // A rating has: { name, artist, rating: { puntuation, id }, mbid}
-  ratings: PropTypes.array,
-  username: PropTypes.string,
-  ratingsProps: PropTypes.array,
+	// A rating has: { name, artist, rating: { puntuation, id }, mbid}
+	ratings: PropTypes.array,
+	username: PropTypes.string,
+	ratingsProps: PropTypes.array,
+	usernameProps: ''
 };
 
 const defaultProps = {
-  ratingsProps: null,
-  ratings: [],
-  username: '',
-}
+	ratingsProps: null,
+	ratings: [],
+	username: ''
+};
 
 function Ratings({ ratings, username, ratingsProps, usernameProps }) {
-  const [albums, setAlbums] = useState([]);
-  const _ratings = ratingsProps || ratings;
-  const _username = usernameProps || username;
-  function albumsSetForEach(album) {
-    if (album) {
-      album.rating = album.ratings ? album.ratings.filter(r => String(r.user) === String(_username))[0] : null;
-    }
-  }
-  useEffect(() => {
-    (async () => {
-      let promiseRating = [];
-      if (_ratings) {
-        for (let rating of _ratings) {
-          promiseRating.push(axios.get(`/api/album/${rating}`));
-        }
-        let albumsSet = (await Promise.all(promiseRating)).map(promise => promise.data);
-        albumsSet.forEach(albumsSetForEach);
-        setAlbums(albumsSet);        
-      }
-    })()
-  }, [ratings, ratingsProps]);
-  const renderError = albums.length === 0 ? (
-    <div className="m-3">
-      <CircularProgress />
-    </div> ) : (
-      <h3>This user hasn't got any ratings yet!</h3>
-    );
-  return (
-    <div>
-      {albums && albums.length > 0 ? albums.map(rating => 
-      (rating && rating.rating ? <Rating
-        albumName={rating.name} 
-        artistName={rating.artist} key={rating._id} 
-        rating={rating.rating.puntuation}
-        mbid={rating.mbid ? rating.mbid : 0}
-        generalScore={10}
-      /> : null)) : <div className="m-3">{renderError}</div> }
-    </div>
-  )
+	const [albums, setAlbums] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const _ratings = ratingsProps || ratings;
+	const _username = usernameProps || username;
+	function setUserRatingOnEachAlbum(album) {
+		if (album) {
+			album.rating = album.ratings
+				? album.ratings.filter(r => String(r.user) === String(_username))[0]
+				: null;
+		}
+	}
+	useEffect(() => {
+		(async () => {
+			let promiseRating = [];
+			if (_ratings) {
+				setLoading(true);
+				for (let rating of _ratings) {
+					promiseRating.push(axios.get(`/api/album/${rating}`));
+				}
+				let albumsSet = (await Promise.all(promiseRating)).map(
+					promise => promise.data
+				);
+				albumsSet.forEach(setUserRatingOnEachAlbum);
+				setAlbums(albumsSet);
+				setLoading(false);
+			}
+		})();
+	}, [ratings, ratingsProps]);
+	const renderError =
+		albums.length === 0 && loading ? (
+			<div className="m-3">
+				<CircularProgress />
+			</div>
+		) : (
+			<h3>This user hasn't got any ratings yet!</h3>
+		);
+	return (
+		<div>
+			{albums && albums.length > 0 ? (
+				albums.map(rating =>
+					rating && rating.rating ? (
+						<Rating
+							albumName={rating.name}
+							artistName={rating.artist}
+							key={rating._id}
+							rating={rating.rating.puntuation}
+							mbid={rating.mbid ? rating.mbid : 0}
+							generalScore={10}
+						/>
+					) : null
+				)
+			) : (
+				<div className="m-3">{renderError}</div>
+			)}
+		</div>
+	);
 }
 
 Ratings.propTypes = propTypes;
 Ratings.defaultProps = defaultProps;
 
-const mapStateToProps = (state) => ({
-  ratings: state.auth.apiUser ? state.auth.apiUser.ratedAlbums : null,
-  username: state.auth.apiUser ? state.auth.apiUser.user : null
+const mapStateToProps = state => ({
+	ratings: state.auth.apiUser ? state.auth.apiUser.ratedAlbums : null,
+	username: state.auth.apiUser ? state.auth.apiUser.user : null
 });
 
-export default connect(mapStateToProps, {})(Ratings)
+export default connect(
+	mapStateToProps,
+	{}
+)(Ratings);
--- client/src/components/Search/SearchRoute/SearchRoute.jsx
+++ client/src/components/Search/SearchRoute/SearchRoute.jsx
@@ -3,151 +3,163 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
-import { searchAlbums, searchArtists, searchPlaylists, searchProfiles } from '../../../actions/searchActions';
+import {
+	searchAlbums,
+	searchArtists,
+	searchPlaylists,
+	searchProfiles
+} from '../../../actions/searchActions';
 import store from '../../../store';
 import Albums from './albums/Albums';
 import Search from '../../../utils/searchFunctions';
 import ArtistsComponent from './artists/Artists.component';
 import Playlists from './playlists/Playlists';
+import Profiles from './profiles/Profiles';
 
 const __propTypes = {
-  search: PropTypes.object.isRequired,
-  searchAlbums: PropTypes.func.isRequired,
-  searchArtists: PropTypes.func.isRequired,
-  searchPlaylists: PropTypes.func.isRequired,
-  searchProfiles: PropTypes.func.isRequired,
+	search: PropTypes.object.isRequired,
+	searchAlbums: PropTypes.func.isRequired,
+	searchArtists: PropTypes.func.isRequired,
+	searchPlaylists: PropTypes.func.isRequired,
+	searchProfiles: PropTypes.func.isRequired
 };
 
 /**
  * @description The search route, handles everything of /search/:query.
  */
 class SearchRoute extends Component {
-  static propTypes = __propTypes;
+	static propTypes = __propTypes;
 
-  constructor() {
-    super();
-    this.state = { albums: { currentItems: 0 }, artists: { currentItems: 0 }, playlists: { currentItems: 0 }, profiles: { currentItems: 0 } };
-    this.searchHandler = null;
-  }
+	constructor() {
+		super();
+		this.state = {
+			albums: { currentItems: 0 },
+			artists: { currentItems: 0 },
+			playlists: { currentItems: 0 },
+			profiles: { currentItems: 0 }
+		};
+		this.searchHandler = null;
+	}
 
-  // REACT LIFECYCLE
+	// REACT LIFECYCLE
 
-  componentWillUnmount() {
-    store.dispatch({ type: 'CLEAN_SEARCH_PAGE' });
-  }
+	componentWillUnmount() {
+		store.dispatch({ type: 'CLEAN_SEARCH_PAGE' });
+	}
 
-  componentDidMount() {
-    const { searchquery } = this.props.match.params;
-    this.searchHandler = new Search(searchquery, store);
-    // We set cleaned: false to inform that we populated lists.
-    store.dispatch({ type: 'POPULATE_SEARCH' });
-    this.searchEverything();
-  }
+	componentDidMount() {
+		const { searchquery } = this.props.match.params;
+		this.searchHandler = new Search(searchquery, store);
+		// We set cleaned: false to inform that we populated lists.
+		store.dispatch({ type: 'POPULATE_SEARCH' });
+		this.searchEverything();
+	}
 
-  componentWillReceiveProps(next) {
-    // Check if the next props are data coming from the bottom bar search.
-    if (next.search.cleaned) {
-      // This means that the user has searched another thing in the searchbar and it has cleaned every lists.
-      // So we need to get the items that the user is searching. So now we set to cleaned: false to say: "Hey, we searched things"
-      store.dispatch({ type: 'POPULATE_SEARCH' });
-      // Change the query param in the Search class.
-      this.searchHandler.searchquery = next.match.params.searchquery;
-      // Populate searchData.
-      this.searchEverything();
-    }
-  }
+	componentWillReceiveProps(next) {
+		// Check if the next props are data coming from the bottom bar search.
+		if (next.search.cleaned) {
+			// This means that the user has searched another thing in the searchbar and it has cleaned every lists.
+			// So we need to get the items that the user is searching. So now we set to cleaned: false to say: "Hey, we searched things"
+			store.dispatch({ type: 'POPULATE_SEARCH' });
+			// Change the query param in the Search class.
+			this.searchHandler.searchquery = next.match.params.searchquery;
+			// Populate searchData.
+			this.searchEverything();
+		}
+	}
 
-   // COMPONENT METHODS
+	// COMPONENT METHODS
 
-  /**
-   * @description Searches every element of the search page.
-   */
-  searchEverything = () => {
-    this.add('albums', this.props.searchAlbums);
-    this.add('artists', this.props.searchArtists);
-    this.add('playlists', this.props.searchPlaylists);
-    this.add('profiles', this.props.searchProfiles);
-  }
+	/**
+	 * @description Searches every element of the search page.
+	 */
+	searchEverything = () => {
+		this.add('albums', this.props.searchAlbums);
+		this.add('artists', this.props.searchArtists);
+		this.add('playlists', this.props.searchPlaylists);
+		this.add('profiles', this.props.searchProfiles);
+	};
 
-  /**
-   * @description Add the items corresponding to their object in the state, and makes the api call.
-   */
-  add = (type, functionprops) => {
-    this.searchHandler.search(
-      type,
-      this.state[type].currentItems + 10,
-      functionprops,
-      1
-    );
-    this.setState({
-      [type]: { currentItems: this.state[type].currentItems + 10 },
-    });
-  };
+	/**
+	 * @description Add the items corresponding to their object in the state, and makes the api call.
+	 */
+	add = (type, functionprops) => {
+		this.searchHandler.search(
+			type,
+			this.state[type].currentItems + 10,
+			functionprops,
+			1
+		);
+		this.setState({
+			[type]: { currentItems: this.state[type].currentItems + 10 }
+		});
+	};
 
-  render() {
-    const { searchquery } = this.props.match.params;
-    const { albums, artists, playlists, profiles } = this.props.search;
-    return (
-      <div className="container">
-        <h1>
-          You searched:{' '}
-          <span className="badge badge-primary">{searchquery}</span>
-        </h1>
-        {/* Albums */}
-        <Albums albums={albums} />
-        <Fab
-          color="primary"
-          aria-label="Add"
-          onClick={() => {
-            this.add('albums', this.props.searchAlbums);
-          }}
-        >
-          <AddIcon />
-        </Fab>
+	render() {
+		const { searchquery } = this.props.match.params;
+		const { albums, artists, playlists, profiles } = this.props.search;
+		return (
+			<div className="container">
+				<h1>
+					You searched:{' '}
+					<span className="badge badge-primary">{searchquery}</span>
+				</h1>
+				{/* Albums */}
+				<Albums albums={albums} />
+				<Fab
+					color="primary"
+					aria-label="Add"
+					onClick={() => {
+						this.add('albums', this.props.searchAlbums);
+					}}
+				>
+					<AddIcon />
+				</Fab>
 
-        {/* Artists */}
-        <ArtistsComponent artists={artists} />
-        <Fab
-          color="primary"
-          aria-label="Add"
-          onClick={() => {
-            this.add('artists', this.props.searchArtists);
-          }}
-        >
-          <AddIcon />
-        </Fab>
-        {/* Playlists */}
-        <Playlists playlists={playlists} />
-        <Fab
-          color="primary"
-          aria-label="Add"
-          onClick={() => {
-            this.add('playlists', this.props.searchPlaylists);
-          }}
-        >
-          <AddIcon />
-        </Fab>
-        {/* TODO: Users */}
-        <Fab
-          color="primary"
-          aria-label="Add"
-          onClick={() => {
-            this.add('profiles', this.props.searchProfiles);
-          }}
-        >
-          <AddIcon />
-        </Fab>
-        <div style={{ paddingBottom: '13%' }} />
-      </div>
-    );
-  }
+				{/* Artists */}
+				<ArtistsComponent artists={artists} />
+				<Fab
+					color="primary"
+					aria-label="Add"
+					onClick={() => {
+						this.add('artists', this.props.searchArtists);
+					}}
+				>
+					<AddIcon />
+				</Fab>
+				{/* Playlists */}
+				<Playlists playlists={playlists} />
+				<Fab
+					color="primary"
+					aria-label="Add"
+					onClick={() => {
+						this.add('playlists', this.props.searchPlaylists);
+					}}
+				>
+					<AddIcon />
+				</Fab>
+				{/* TODO: Users */}
+				<Profiles profiles={profiles} />
+				<Fab
+					color="primary"
+					aria-label="Add"
+					onClick={() => {
+						this.add('profiles', this.props.searchProfiles);
+					}}
+				>
+					<AddIcon />
+				</Fab>
+				<div style={{ paddingBottom: '13%' }} />
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  search: state.search.searchData,
+	search: state.search.searchData
 });
 
 export default connect(
-  mapStateToProps,
-  { searchAlbums, searchArtists, searchPlaylists, searchProfiles }
+	mapStateToProps,
+	{ searchAlbums, searchArtists, searchPlaylists, searchProfiles }
 )(SearchRoute);
