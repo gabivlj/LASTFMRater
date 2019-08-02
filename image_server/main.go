5f596ff41742c3fae0a047f779a615246ebf45b7
gabivlj <gabitriqui@gmail.com>
2019-08-02 01:50:48 +0100

    Token .env checking from the go server on Post or future socket requests

--- image_server/main.go
+++ image_server/main.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	server "github.com/gabivlj/grumpy_image_server/server"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println(".env file not found.")
+		return
+	}
+
+	ok := false
+	server.Key, ok = os.LookupEnv("CLIENT_KEY")
+
+	if !ok {
+		fmt.Println("not ok")
+		return
+	}
+
 	port := "2222"
 	router := mux.NewRouter()
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Origin", "Content-Type", "X-Auth-Token", "Authorization"})
+	router.Use(server.TokenAuth)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Origin", "Content-Type", "X-Auth-Token", "Authorization", "CLIENT_KEY"})
 	originsOk := handlers.AllowedOrigins([]string{"*"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
