df1ab3d86d51496ed7561290c854470deac46fbe
gabivlj <gabitriqui@gmail.com>
2019-09-18 23:29:49 +0200

    getRecommendedFollowers query more or less done...

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -3,5 +3,5 @@ module.exports = {
    * @param {Object} app, express
    * @param {Array} route, [routeString, router]
    */
-  addRoutes: (app, ...args) => args.forEach(arg => app.use(arg[0], arg[1]))
+  addRoutes: (app, ...args) => args.forEach(arg => app.use(arg[0], arg[1])),
 };
