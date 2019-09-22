c5e7148c2f3f9012617fff10576046ab8ac1b849
gabivlj <gabitriqui@gmail.com>
2019-09-22 19:34:28 +0200

    Fixed comment section when opening from gramps, created the useComment hook that we may recicle in the future...

--- client/src/components/Comment/CommentDialog.jsx
+++ client/src/components/Comment/CommentDialog.jsx
@@ -8,6 +8,7 @@ import {
 } from '../../actions/commentActions';
 import DialogMe from '../Chat/Chat/Dialog/Dialog';
 import CommentRender from './CommentRender';
+import useComments from '../../hooks/useComments';
 
 function CommentDialog({
   commentOverlay,
@@ -17,56 +18,15 @@ function CommentDialog({
   getComments,
   cleanComments,
 }) {
-  const { showCommentOverlay, comment, loaded } = comments;
-  const [, setCurrentNOfComments] = useState();
-
-  const numberOfCommentsAdd = 50;
-  function checkBottom() {
-    // When scrolling to the bottom of the component, reload comments.
-    let timeoutForLoading = false;
-    return e => {
-      if (
-        e.target.scrollTop >=
-          e.target.scrollHeight - e.target.clientHeight - 10 &&
-        loaded &&
-        !timeoutForLoading
-      ) {
-        setLoading();
-        // We do the update of the comments here because otherwise I don't know how we will get the prev value.
-        setCurrentNOfComments(prev => {
-          getComments(
-            comment._id,
-            0,
-            prev + numberOfCommentsAdd,
-            auth.apiUser ? auth.apiUser.id : null,
-          );
-          // Tell the browser not to load again in 2s.
-          timeoutForLoading = true;
-          setTimeout(() => {
-            timeoutForLoading = false;
-          }, 2000);
-          // Update.
-          return prev + numberOfCommentsAdd;
-        });
-      }
-    };
-  }
-
-  useEffect(() => {
-    if (comment) {
-      setLoading();
-      setCurrentNOfComments(prev => {
-        getComments(
-          comment._id,
-          0,
-          prev + numberOfCommentsAdd,
-          auth.apiUser ? auth.apiUser.id : null,
-        );
-        return prev + numberOfCommentsAdd;
-      });
-    }
-    return () => setCurrentNOfComments(0);
-  }, [comment]);
+  const { showCommentOverlay, comment } = comments;
+  const [checkBottom] = useComments(
+    setLoading,
+    getComments,
+    comments,
+    auth,
+    2000,
+    50,
+  );
 
   return (
     <div>
@@ -83,7 +43,7 @@ function CommentDialog({
         propsRender={{ comment }}
         renderActions="a"
         scrollableGeneral={false}
-        onScroll={checkBottom()}
+        onScroll={checkBottom}
       />
     </div>
   );
---
+++ client/src/hooks/useComments.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+/**
+ *
+ * @param {*} setLoading
+ * @param {*} getComments
+ * @param {*} comments
+ * @param {*} auth
+ * @param {*} timeoutMS
+ * @param {*} numberOfCommentsAdd
+ * @param {*} preloadSomeComments
+ * @description useComments returns the checkBottom function to check whenever it hits bottom of the web and it loads more comments.
+ * @returns {Function} checkBottom
+ */
+export default function useComments(
+  setLoading,
+  getComments,
+  comments,
+  auth,
+  timeoutMS = 2000,
+  numberOfCommentsAdd = 50,
+  preloadSomeComments = true,
+) {
+  const { comment, loaded } = comments;
+  const [, setCurrentNOfComments] = useState(0);
+  let timeoutForLoading = false;
+  const [check, setCheck] = useState(function() {});
+  useEffect(() => {
+    function checkBottom() {
+      // When scrolling to the bottom of the component, reload comments.
+      return e => {
+        if (
+          e.target.scrollTop >=
+            e.target.scrollHeight - e.target.clientHeight - 10 &&
+          loaded &&
+          !timeoutForLoading
+        ) {
+          setLoading();
+          // We do the update of the comments here because otherwise I don't know how we will get the prev value.
+          setCurrentNOfComments(prev => {
+            getComments(
+              comment._id,
+              0,
+              prev + numberOfCommentsAdd,
+              auth.apiUser ? auth.apiUser.id : null,
+            );
+            // Tell the browser not to load again in 2s.
+            timeoutForLoading = true;
+            setTimeout(() => {
+              timeoutForLoading = false;
+            }, timeoutMS);
+            // Update.
+            return prev + numberOfCommentsAdd;
+          });
+        }
+      };
+    }
+    setCheck(() => checkBottom());
+  }, [comment._id]);
+  useEffect(() => {
+    if (comment) {
+      setLoading();
+      setCurrentNOfComments(prev => {
+        getComments(
+          comment._id,
+          0,
+          prev + numberOfCommentsAdd,
+          auth.apiUser ? auth.apiUser.id : null,
+        );
+        return prev + numberOfCommentsAdd;
+      });
+    }
+    return () => setCurrentNOfComments(0);
+  }, [comment]);
+
+  return [check];
+}
